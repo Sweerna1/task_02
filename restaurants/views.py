e691f422544cbcb736c6aae7171e06709c41cc91
Sarah <sweerna1@gmail.com>
2019-08-20 13:52:46 +0300

    Dawwood_Sarah

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def home(request):
+
+	context ={
+	"msg":"Hello World!"
+	}
+
+	return render(request,'html_file.html',context)
